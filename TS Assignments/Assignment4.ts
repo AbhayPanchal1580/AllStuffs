c15228dccc414233e0c6a8d540a5bf73979ac261
Panchal, Abhay <abhay.panchal@emtecinc.com>
2022-09-07 17:54:19 +0530

    Ts upto 15

--- TS Assignments/Assignment4.ts
+++ TS Assignments/Assignment4.ts
@@ -1,71 +1,143 @@
-// write typescript code with following requirements (design proper classes). The requirements have
-// suggestions for fields and methods, you are free to add more as per your thinking
-
-// Bank Account
-// fields: id, firstName, lastName, address, phone, email, type (saving/current)
-// methods: createAccount, updateAccount, deleteAccount
-
-// Transaction
-// fields: date, type, amount, customerId
-// methods: depositFunds, withdrawFunds
-
 interface Bank {
+    id?: number;
+    firstName?: string;
+    lastName?: string;
+    address?: string;
+    phone?: number;
+    email?: string;
+    type?: string;
+  
+    createAccount(
+      id?: number,
+      firstName?: string,
+      lastName?: string,
+      address?: string,
+      phone?: number,
+      email?: string,
+      type?: string
+    ): void;
+    updateAccount(num:number,val:string): any;
+    deleteAccount(num:number): any;
+  }
+  
+
+
+class BankAccount {
+
     id: number;
+
     firstName: string;
+
     lastName: string;
+
     address: string;
-    phone: number;
+
+    phone: string;
+
     email: string;
+
     type: string;
 
-    createAccount(
-        id: number,
-        firstName: string,
-        lastName: string,
-        address: string,
-        phone: number,
-        email: string,
-        type: string
-    ): void;
-    updateAccount(): any;
-    deleteAccount(): any;
+    Accounts = [{
+
+        id: 0,
+
+        firstName: '',
+
+        lastName: '',
+
+        address: '',
+
+        phone: '',
+
+        email: "",
+
+        type: ''
+
+    }];
+
+    constructor(id: number = 0, firstName: string = "", lastName: string = "", address: string = ""
+
+        , phone: string = "", email: string = "", type: string = "") {
+
+        this.id = id;
+
+        this.firstName = firstName;
+
+        this.lastName = lastName;
+
+        this.address = address;
+
+        this.phone = phone;
+
+        this.email = email;
+
+        this.type = type;
+
+    }
+
+    createAccount(id: number, firstName: string, lastName: string, address: string
+        , phone: string, email: string, type: string) {
+        this.Accounts.push({ id, firstName, lastName, address, phone, email, type });
+    }
+    updateAccount(num: number, val: string) {
+        for (let i = 0; i < this.Accounts.length; i++) {
+            if (this.Accounts[i].id == num) {
+                this.Accounts[i].phone = val;
+            }
+        }
+    }
+
+    deleteAccount(num: number) {
+        for (let i = 0; i < this.Accounts.length; i++) {
+            if (this.Accounts[i].id == num) {
+                this.Accounts.splice(i, 1);
+            }
+        }
+    }
 }
 
-class BankAccount implements Bank {
-    createAccount = (
-        id: number,
-        firstName: string,
-        lastName: string,
-        address: string,
-        phone: number,
-        email: string,
-        type: string
-    ): void => {
-        (this.id = id),
-            (this.firstName = firstName),
-            (this.lastName = lastName),
-            (this.address = address),
-            (this.phone = phone),
-            (this.email = email),
-            (this.type = type);
-    };
+const a1 = new BankAccount();
 
-    updateAccount() {
-        throw new Error("Method not implemented.");
+a1.createAccount(1, 'Abhi', 'Panchal', 'Pune', '9146334963', 'abhi@gmail', 'Savings');
+
+a1.createAccount(2, 'Suyash', 'Shinde', 'Nilanga', '9987564123', 'suyog@gmail', 'current');
+
+a1.deleteAccount(1);
+
+a1.updateAccount(2,'9850800961');
+
+console.log(a1.Accounts);
+
+class Transaction extends BankAccount {
+
+    data: string
+
+    type: string
+
+    amount: number;
+
+    customerId: string
+
+    constructor(data: string, type: string, amount: number, customerId: string) {
+
+        super();
+
+        this.data = data;
+
+        this.type = type;
+
+        this.amount = amount;
+
+        this.customerId = customerId;
+
     }
-    deleteAccount() {
-        throw new Error("Method not implemented.");
-    }
+
+    // depositeFunds()
+
 }
 
-const person1 = new BankAccount(
-    1,
-    "abhay",
-    "panchal",
-    "Nilanga",
-    9146334963,
-    "abhay@gmail.com",
-    "saving"
-);
+    // widrowFunds(){
 
-console.log(person1);
+
+
--- TS Assignments/js/Assignment4.js
+++ TS Assignments/js/Assignment4.js
@@ -1,24 +1,54 @@
 "use strict";
-// write typescript code with following requirements (design proper classes). The requirements have
-// suggestions for fields and methods, you are free to add more as per your thinking
 class BankAccount {
-    constructor() {
-        this.createAccount = (id, firstName, lastName, address, phone, email, type) => {
-            (this.id = id),
-                (this.firstName = firstName),
-                (this.lastName = lastName),
-                (this.address = address),
-                (this.phone = phone),
-                (this.email = email),
-                (this.type = type);
-        };
+    constructor(id = 0, firstName = "", lastName = "", address = "", phone = "", email = "", type = "") {
+        this.Accounts = [{
+                id: 0,
+                firstName: '',
+                lastName: '',
+                address: '',
+                phone: '',
+                email: "",
+                type: ''
+            }];
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.phone = phone;
+        this.email = email;
+        this.type = type;
     }
-    updateAccount() {
-        throw new Error("Method not implemented.");
+    createAccount(id, firstName, lastName, address, phone, email, type) {
+        this.Accounts.push({ id, firstName, lastName, address, phone, email, type });
     }
-    deleteAccount() {
-        throw new Error("Method not implemented.");
+    updateAccount(num, val) {
+        for (let i = 0; i < this.Accounts.length; i++) {
+            if (this.Accounts[i].id == num) {
+                this.Accounts[i].phone = val;
+            }
+        }
+    }
+    deleteAccount(num) {
+        for (let i = 0; i < this.Accounts.length; i++) {
+            if (this.Accounts[i].id == num) {
+                this.Accounts.splice(i, 1);
+            }
+        }
     }
 }
-const person1 = new BankAccount(1, "abhay", "panchal", "Nilanga", 9146334963, "abhay@gmail.com", "saving");
-console.log(person1);
+const a1 = new BankAccount();
+a1.createAccount(1, 'Abhi', 'Panchal', 'Pune', '9146334963', 'abhi@gmail', 'Savings');
+a1.createAccount(2, 'Suyash', 'Shinde', 'Nilanga', '9987564123', 'suyog@gmail', 'current');
+a1.deleteAccount(1);
+a1.updateAccount(2, '9850800961');
+console.log(a1.Accounts);
+class Transaction extends BankAccount {
+    constructor(data, type, amount, customerId) {
+        super();
+        this.data = data;
+        this.type = type;
+        this.amount = amount;
+        this.customerId = customerId;
+    }
+}
+// widrowFunds(){
